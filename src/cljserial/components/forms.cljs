7a7d1c826c063f0cf6ab1946e646599b4372f426
Cormac Cannon <cormacc@gmail.com>
2024-03-05 10:16:08 +0000

    feat: Updated 'select' builder to take an optional value extractor
    The dropdown / 'select' builder now takes optional 'format' and 'value'
    keys in the argument map, to format the dropdown element description for
    display and extract a value, respectively.
    
    The default implemenentations just passthrough the element.

--- src/cljserial/components/forms.cljs
+++ src/cljserial/components/forms.cljs
@@ -3,11 +3,11 @@
    [uix.core :refer [defui $]]))
 
 (defui select
-  [{:keys [items selected format disabled on-change] :or {selected nil format (fn [item] item) disabled false}}]
+  [{:keys [items selected format value disabled on-change] :or {selected nil format (fn [item] item) value format disabled false}}]
   ($ :select.rounded-md.w-full.h-6.py-0.text-xs
-     {:default-value (format selected)
+     {:default-value (value selected)
       :disabled disabled
       :on-change on-change}
      (map-indexed
-      (fn [idx item] ($ :option {:key idx} (format item)))
+      (fn [idx item] ($ :option {:key idx :value (value item)} (format item)))
       items)))
